a81136cf8ec391bb12cd78c6acb8be0c5261e412
katakarn supserm <katakarn@192.168.1.5>
2022-04-24 12:05:31 +0700

    test: fix app.component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('odds-coming-soon app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('coming soon by @katakarn');
   });
 });
